63c26cd1260dac10fad0f3fff4af1f64bd9e14a5
JJHaEun <yj05037@gmail.com>
2023-07-03 16:45:32 +0900

    form의 기본동작 막고 입력된값console.log에 뿌리기

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <form action="">
+  <form action="" @submit="login">
     <section>
       <label for="userid">아이디</label>
       <input id="userid" type="text" v-model="username" />
@@ -19,6 +19,14 @@ export default {
       username: "",
       password: "",
     };
+  },
+  methods: {
+    login(e) {
+      // 폼 내의 값을 이용해 서버에 보낸다.
+      // 폼의 기본동작 ==> 새로고침
+      e.preventDefault(); // 폼 기본동작 막기
+      console.log(this.username, this.password);
+    },
   },
 };
 </script>
